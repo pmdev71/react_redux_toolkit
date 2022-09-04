775e3b1903e718d8b3d6114b697bdb3cd34806c1
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-04 14:45:03 +0600

    React Redux Counter App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
-import Profile from './components/Profile';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <h1>Welcome to Redux Counter App</h1>
+      <Counter />
+      <h2>--------------------------------------------------------</h2>
+      <div>
+        <h4>1. state - count : 0 </h4>
+
+        <h4>2. action - increment, decrement, reset </h4>
+
+        <h4>
+          3. reducer - function that takes state and action as arguments and
+          returns a new state.{' '}
+        </h4>
+        <h4>- incriment -> count => count + 1 </h4>
+        <h4>- decriment -> count => count - 1 </h4>
+        <h4>- reset -> count => 0 </h4>
+
+        <h4>4. store - globalized state </h4>
+        <h4>5. Provider - wrap the index.js with Provider </h4>
+        <h4>6. Use store</h4>
+        <h4>- useSelector - For access the state value from store. </h4>
+        <h4>-useDispatch - For dispatch the action to reducer. </h4>
+
+        <h4>npm i redux</h4>
+        <h4>-package for create store </h4>
+
+        <h4>npm i react-redux </h4>
+        <h4>-package for connect react with redux </h4>
+
+        <h4>npm i redux-thunk</h4>
+        <h4>-package for async action </h4>
+
+        <h4>npm i redux-devtools-extension </h4>
+        <h4>-package for redux dev tools</h4>
+      </div>
     </div>
   );
 }
---
+++ src/services/actions/counterAction.js
@@ -0,0 +1,41 @@
+import {
+  DECREMENT_COUNTER,
+  INCREMENT_COUNTER,
+  RESET_COUNTER,
+} from '../constants/counterConstant';
+
+export const incrementCounter = () => {
+  return {
+    type: INCREMENT_COUNTER,
+  };
+};
+
+export const decrementCounter = () => {
+  return {
+    type: DECREMENT_COUNTER,
+  };
+};
+
+export const resetCounter = () => {
+  return {
+    type: RESET_COUNTER,
+  };
+};
+
+// 1. state - count : 0
+// 2. action - increment, decrement, reset
+// 3. reducer - function that takes state and action as arguments and returns a new     state.
+//         - incriment -> count => count + 1
+//         - decriment -> count => count - 1
+//         - reset -> count => 0
+// 4. store - globalized state
+// 5. Provider - wrap the index.js with Provider
+// 6. Use store
+//         - useSelector - For access the state value from store.
+//         - useDispatch - For dispatch the action to reducer.
+
+// npm i redux -->package for create store
+// npm i react-redux -->package for connect react with redux
+
+// npm i redux-thunk -->package for async action
+// npm i redux-devtools-extension -->package for redux dev tools
